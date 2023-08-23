c834e0a43abd311f90ee106d9e01357fbc111786
soniqmidd <soniqmidd@users.noreply.github.com>
2023-08-22 19:44:35 -0500

    Added the search engine

--- src/script.js
+++ src/script.js
@@ -46,7 +46,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "ffb1fa2c90cad31d90a4daa88adc6920";
-let city = "Corpus Christi";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "ffb1fa2c90cad31d90a4daa88adc6920";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+//controls search info
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
