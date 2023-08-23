4ca1118d819d1be1a6227a0bbf6a6cfcb47a4ba8
soniqmidd <soniqmidd@users.noreply.github.com>
2023-08-22 22:16:53 -0500

    Implemented the unit conversions

--- src/script.js
+++ src/script.js
@@ -33,7 +33,9 @@ function displayTemperature(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -46,6 +48,7 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+//search a city worldwide
 function search(city) {
   let apiKey = "ffb1fa2c90cad31d90a4daa88adc6920";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -58,8 +61,39 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("New York");
+//display fahrenheit temperature
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  //add the active class from celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+//display back to celsius temperature
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  //add the active class from fahrenheit link
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 //controls search info
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+//switch to fahrenheit metric
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+//switch back to celsius metric
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
